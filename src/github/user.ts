ce3a6a50b71d08dd830c7df328f6d86fb9df5285
6loccee <martiin17jr@gmail.com>
2024-07-06 23:52:30 +0200

    feat: add news validators

--- src/github/user.ts
+++ src/github/user.ts
@@ -3,6 +3,11 @@ import { base_url } from '../utils'
 export const userData = async (username: string) => {
 	const url = `${base_url}/users/${username}`
 	const response = await fetch(url)
+	
+	if(!response.ok) {
+		console.error('Failed to fetch user')
+		process.exit(1)
+	}
 	const {
 		id,
 		name,
