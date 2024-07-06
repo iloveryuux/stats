b00c1b6ff93a72e3de443c7e0cf952dd060edd82
6loccee <martiin17jr@gmail.com>
2024-07-06 05:59:27 +0200

    feat: first version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log("Hello via Bun!");
+import Table from 'cli-table3'
+const args = process.argv.slice(2)
+
+if(args.length < 2) {
+  console.error(`[-] Use: <username> <repo>`)
+  process.exit(1)
+}
+
+const userGithub = args[0]
+const repo = args[1]
+
+const base_url = `https://api.github.com/repos/${userGithub}/${repo}`
+const response = await fetch(base_url)
+
+const { name, description, stargazers_count, forks_count } =
+	await response.json()
+
+const table = new Table({
+	head: ['Name', 'Description', ' Stars', ' Forks'],
+  style: {
+    head: ['cyan'],
+  }
+})
+
+table.push([
+	name,
+	truncateText(description || 'No description available'),
+	stargazers_count,
+	forks_count,
+])
+
+console.log(table.toString())
+
+function truncateText(description: string) {
+  if(description.length > 30) {
+    return description.substring(0, 30) + '...'
+  }
+  return description
+}
